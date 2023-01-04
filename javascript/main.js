829a9957a1dfbdf618ff66eb6d92880d96eda0e2
Samuelhzx <lchbhh@163.com>
2023-01-04 09:12:45 +0800

    add things

---
+++ javascript/main.js
@@ -0,0 +1,17 @@
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';/*
+document.querySelector("html").addEventListener("click", function () {
+    alert("别戳我，我怕疼。");
+});*/
+let myImage = document.querySelector('img');
+
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === '../images/ou.svg') {
+        myImage.setAttribute('src', '../images/yan.svg');
+    } else if (mySrc === '../images/yan.svg') {
+        myImage.setAttribute('src', '../images/liu.svg');
+    } else {
+        myImage.setAttribute('src', '../images/ou.svg');
+    }
+}
