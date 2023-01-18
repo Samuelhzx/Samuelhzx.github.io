a6e9d9a5847a1fadc4e2f03142a01a36cff3a86d
Samuelhzx <lchbhh@163.com>
2023-01-18 15:41:29 +0800

    更多raandpic

--- javascript/randpic.js
+++ javascript/randpic.js
@@ -6,15 +6,38 @@ input.focus()
 button.addEventListener('click', reply);
 
 function reply() {
+	//处理文本
 	writed = input.value.split(' ')
+	for (i = 0; i < writed.length; i++) {
+		if (writed[i] === '') {
+			writed.splice(i, 1);
+			i--;
+		}
+	}
+
 	if (writed[0] === 'article') {
-		if (writed.length === 1) {
+
+		if (writed.length !== 2) {
 			replytext.textContent = '用法：article <人物名>';
 		} else {
 			replytext.textContent = '有一天，bai和' + writed[1] +
 				'一起出去散步。他们度过了愉快的一天，而且没有得新冠，真幸运！';
 		}
+
+	} else if (writed[0] === '压缩毛巾') {
+
+		if (writed.length !== 3) {
+			replytext.textContent = '用法：压缩毛巾 名称 不是什么。如: 压缩毛巾 压缩毛巾 饼干';
+
+		} else {
+			replytext.textContent = '什么，这不是' +writed[1]+ '，我们这个'+writed[2]+'体积小方便携带，拆开一包，放水里就变大，\
+							怎么扯都扯不坏，用来擦脚，擦脸，擦嘴都是很好用的，你看打开以后像圆饼一样大小，\
+							放在水里遇水变大变高，吸水性很强的。打开以后，是一条加大加厚的毛巾，你看他怎么挣都挣不坏，\
+							好不掉毛不掉絮，使用七八次都没问题，出差旅行带上它非常方便，用它擦擦脚，再擦擦嘴，\
+							擦擦脸，干净卫生。什么 ? 在哪里买 ? 下方小黄车，买五包送五包，还包邮';
+		}
+
 	} else {
-		replytext.textContent = '功能：\narticle\n(randpic还没实现)';
+		replytext.textContent = '功能：\narticle\n压缩毛巾\n(randpic还没实现)';
 	}
 }
