c200bf91c632944a2157ff3a99e9d64df4d002dc
Samuelhzx <lchbhh@163.com>
2023-01-26 20:19:48 +0800

    抢21 v0.0.0 发布！

--- javascript/qiang21.js
+++ javascript/qiang21.js
@@ -1,30 +1,58 @@
 const qiangSubmit = document.getElementById('qiangSubmit');
 const qiangInput = document.getElementById('qiangInput');
-const reply = document.querySelectorAll('.reply');
-const botReply = document.querySelector('.reply');
+const botFirstReply = document.querySelector('.botreply');
 const bot = document.getElementById('bot');
-let num = 1;
 const jishu = document.getElementById('jishu');
-jishu.textContent = num;
 
-let playerName = '无名大侠'
+let num = 1;
+jishu.textContent = num;
+let playerName = '无名大侠';
 if (localStorage.getItem('name')) {
     playerName = localStorage.getItem('name');
 }
-playerName += ':'
+//playerName += ':'
 
-botReply.textContent = '我先说，1';
+botFirstReply.textContent = '我先说，1';
 qiangSubmit.addEventListener('click', submitFunction);
 
 function submitFunction() {
-    num += Number(qiangInput.value);
-    jishu.textContent = num;
+    let replynum = Number(qiangInput.value);
     let playerNameDisplay = document.createElement('p');
     let playerSubmit = document.createElement('p');
     playerSubmit.className = 'reply';
+    playerNameDisplay.style.textAlign = 'right';
     playerNameDisplay.textContent = playerName;
     playerSubmit.textContent = qiangInput.value;
     bot.appendChild(playerNameDisplay);
     bot.appendChild(playerSubmit);
-    
+
+    let kong = document.createElement('br');
+    bot.appendChild(kong);
+
+    let botNameDisplay = document.createElement('p');
+    let botSubmit = document.createElement('p');
+    botSubmit.className = 'botreply';
+    botNameDisplay.textContent = '机器人';
+    bot.appendChild(botNameDisplay);
+
+    if (replynum >= 1 && replynum <= 3 && replynum % 1 === 0) {
+        num += replynum;
+        let botReplyNumber = 4 - (num - 1) % 4;
+        botSubmit.textContent = botReplyNumber;
+        num += botReplyNumber;
+        jishu.textContent = num;
+    } else {
+        botSubmit.textContent = '输入不符合规则';
+    }
+    bot.appendChild(botSubmit);
+
+    if (num === 21) {
+        botNameDisplay = document.createElement('p');
+        botSubmit = document.createElement('p');
+        botSubmit.className = 'botreply';
+        botNameDisplay.textContent = '机器人';
+        botSubmit.textContent = '我赢了';
+        bot.appendChild(botNameDisplay);
+        bot.appendChild(botSubmit);
+    }
 }
