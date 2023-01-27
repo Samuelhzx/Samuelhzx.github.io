99df6e26a32af0331fd0452f50ead73963d45114
Samuelhzx <lchbhh@163.com>
2023-01-27 15:54:11 +0800

    抢21 v0.0.4 发布！

--- javascript/qiang21.js
+++ javascript/qiang21.js
@@ -16,6 +16,25 @@ botFirstReply.textContent = '我先说，1';
 qiangSubmit.addEventListener('click', submitFunction);
 
 function submitFunction() {
+    function botsay(word) {
+        let botNameDisplay = document.createElement('p');
+        let botSubmit = document.createElement('p');
+        botSubmit.className = 'botreply';
+        botNameDisplay.textContent = '机器人';
+        botSubmit.textContent = word;
+        bot.appendChild(botNameDisplay);
+        bot.appendChild(botSubmit);
+    }
+    function end() {
+        qiangInput.disabled = true;
+        qiangSubmit.disabled = true;
+        qiangSubmit.onmouseover = bubian();
+        function bubian() {
+            qiangSubmit.style.backgroundColor = 'rgba(0, 200, 200, 1)';
+            qiangSubmit.style.cursor = 'not-allowed';
+            qiangSubmit.style.boxShadow = '0px 0px 0px';
+        }
+    }
     let replynum = Number(qiangInput.value);
 
     let playerNameDisplay = document.createElement('p');
@@ -29,48 +48,36 @@ function submitFunction() {
 
     bot.innerHTML += '<br><br>'
 
-    let botNameDisplay = document.createElement('p');
-    let botSubmit = document.createElement('p');
-    botSubmit.className = 'botreply';
-    botNameDisplay.textContent = '机器人';
-    bot.appendChild(botNameDisplay);
+    let botReplyWord = ''
 
     if (replynum >= 1 && replynum <= 3 && replynum % 1 === 0) {
         num += replynum;
         let botReplyNumber = 4 - (num - 1) % 4;
-        botSubmit.textContent = botReplyNumber;
+        botReplyWord = botReplyNumber;
         num += botReplyNumber;
         jishu.textContent = num;
     } else {
         wrong += 1;
         if (wrong === 10) {
-            botSubmit.textContent = '再这样我不玩了';
+            botReplyWord = '再这样我不玩了';
         } else if (wrong === 11) {
-            botSubmit.textContent = '我不玩了';
-            botSubmit.textContent = '再见';
-            qiangInput.disabled = true;
-            qiangSubmit.disabled = true;
+            botReplyWord = '我不玩了，再见';
+            end();
         } else if (Math.random() < 1 / 3) {
-            botSubmit.textContent = '输入不符合规则';
+            botReplyWord = '输入不符合规则';
         } else if (Math.random() < 1 / 2) {
-            botSubmit.textContent = '故意找茬是不是？';
+            botReplyWord = '故意找茬是不是？';
         } else {
-            botSubmit.textContent = '你玩不玩吧';
+            botReplyWord = '你玩不玩吧';
         }
 
     }
-    bot.appendChild(botSubmit);
-
+    botsay(botReplyWord);
     if (num === 21) {
-        botNameDisplay = document.createElement('p');
-        botSubmit = document.createElement('p');
-        botSubmit.className = 'botreply';
-        botNameDisplay.textContent = '机器人';
-        botSubmit.textContent = '我赢了';
-        bot.appendChild(botNameDisplay);
-        bot.appendChild(botSubmit);
-        qiangInput.disabled = true;
-        qiangSubmit.disabled = true;
+        botReplyWord = '我赢了';
+        botsay(botReplyWord);
+        end();
     }
+
     bot.scrollTop = bot.scrollHeight;
 }
