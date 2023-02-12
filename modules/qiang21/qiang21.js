39dac561c12b94886dd2acce769da32dda9116dc
Samuelhzx <lchbhh@163.com>
2023-02-13 01:41:07 +0800

    改了布局

---
+++ modules/qiang21/qiang21.js
@@ -0,0 +1,88 @@
+const qiangSubmit = document.getElementById('qiangSubmit');
+const qiangInput = document.getElementById('qiangInput');
+const botFirstReply = document.querySelector('.botreply');
+const bot = document.getElementById('bot');
+const jishu = document.getElementById('jishu');
+let wrong = 0;
+let num = 1;
+jishu.textContent = num;
+let playerName = '无名大侠';
+if (localStorage.getItem('name')) {
+    playerName = localStorage.getItem('name');
+}
+//playerName += ':'
+
+botFirstReply.textContent = '我先说，1';
+qiangSubmit.onclick = submitFunction;
+
+function submitFunction() {
+    if (qiangInput.value === '') {
+        alert('没输入');
+    } else {
+        function botsay(word) {
+            let botNameDisplay = document.createElement('p');
+            let botSubmit = document.createElement('p');
+            botSubmit.className = 'botreply';
+            botNameDisplay.textContent = '机器人';
+            botSubmit.textContent = word;
+            bot.appendChild(botNameDisplay);
+            bot.appendChild(botSubmit);
+        }
+
+        function end() {
+            qiangInput.disabled = true;
+            qiangSubmit.disabled = true;
+            qiangSubmit.onmouseover = bubian();
+            function bubian() {
+                qiangSubmit.style.backgroundColor = 'rgba(0, 200, 200, 1)';
+                qiangSubmit.style.cursor = 'not-allowed';
+                qiangSubmit.style.boxShadow = '0px 0px 0px';
+            }
+        }
+
+        let replynum = Number(qiangInput.value);
+
+        let playerNameDisplay = document.createElement('p');
+        let playerSubmit = document.createElement('p');
+        playerSubmit.className = 'reply';
+        playerNameDisplay.style.textAlign = 'right';
+        playerNameDisplay.textContent = playerName;
+        playerSubmit.textContent = qiangInput.value;
+        bot.appendChild(playerNameDisplay);
+        bot.appendChild(playerSubmit);
+
+        bot.innerHTML += '<br><br>'
+
+        let botReplyWord = ''
+
+        if (replynum >= 1 && replynum <= 3 && replynum % 1 === 0) {
+            num += replynum;
+            let botReplyNumber = 4 - (num - 1) % 4;
+            botReplyWord = botReplyNumber;
+            num += botReplyNumber;
+            jishu.textContent = num;
+        } else {
+            wrong += 1;
+            if (wrong === 10) {
+                botReplyWord = '再这样我不玩了';
+            } else if (wrong === 11) {
+                botReplyWord = '我不玩了，再见';
+                end();
+            } else if (Math.random() < 1 / 3) {
+                botReplyWord = '输入不符合规则';
+            } else if (Math.random() < 1 / 2) {
+                botReplyWord = '故意找茬是不是？';
+            } else {
+                botReplyWord = '你玩不玩吧';
+            }
+
+        }
+        botsay(botReplyWord);
+        if (num === 21) {
+            botsay('我赢了');
+            end();
+        }
+
+        bot.scrollTop = bot.scrollHeight;
+    }
+}
---
+++ modules/space/space.js
@@ -0,0 +1,37 @@
+const namebutton = document.getElementById("rename");
+const title = document.getElementById('title');
+const htmltitle = document.querySelector('title');
+const removename = document.getElementById('removename');
+
+htmltitle.textContent = '个人主页';
+
+namebutton.onclick = updateName;
+
+function updateName() {
+	let name = prompt('输入昵称：');
+	if (name == "") {
+		name = '无名大侠';
+	}
+	if (name !== null) {
+		localStorage.setItem('name', name);
+		title.textContent = name;
+		htmltitle.textContent = name + '的个人主页';
+	}
+}
+
+if (localStorage.getItem('name')) {
+	let storedName = localStorage.getItem('name');
+	title.textContent = storedName;
+	htmltitle.textContent = storedName + '的个人主页';
+}
+
+removename.onclick = removeName;
+
+function removeName() {
+	const ask = confirm('确定注销？')
+	if (ask) {
+		localStorage.removeItem('name');
+		title.textContent = '无名大侠';
+		htmltitle.textContent = '个人主页';
+	}
+}
