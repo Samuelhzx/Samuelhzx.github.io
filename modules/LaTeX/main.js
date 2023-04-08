df0581b00102b1cea03153ef1fabfc8cda3f7715
Samuelhzx <lchbhh@163.com>
2023-04-08 17:47:53 +0800

    增加实时渲染latex的功能

--- modules/LaTeX/main.js
+++ modules/LaTeX/main.js
@@ -1,13 +1,24 @@
 let clipboardObj = navigator.clipboard;
 let o = document.querySelector('.output');//非这样不可
+o.disabled = true;
 let input;
 if (clipboardObj) {
-	$(".btn").on('click', async function () {
+	$(".copy").on('click', async function () {
 		await navigator.clipboard.writeText(o.innerText)
 	})
 }
 
 $('textarea').on('keyup', () => {
-	input = document.querySelector('textarea').value;
-	o.innerText = input;
+	let input = document.querySelector('textarea').value;
+	if (input !== "") {
+		o.innerText = input;
+	} else {
+		o.innerText = '输出区域';
+	}
+	mathjaxfy(input);
 })
+
+function mathjaxfy(text) {
+	$('.mathjaxed').text('\\('+text+'\\)');
+	MathJax.typeset();
+}
